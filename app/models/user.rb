42ab30ff8a9da2e166230bf6a74629857a57c2c0
Gary Richards <garyr@opensourcery.uk>
2026-02-12 20:20:34 +0000

    Initial web talker example

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+  has_many :sent_messages, as: :sender, class_name: "Message"
+  has_many :received_messages, as: :receiver, class_name: "Message"
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  def messages
+    Message.visible_to(self)
+  end
 end
